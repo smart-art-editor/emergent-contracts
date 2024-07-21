7caff861f266bd3f671b60db3f5b7b2f896bc4fa
ANNMAINAWANGARI <wangariann371@gmail.com>
2024-07-22 02:10:33 +0300

    modified createWorkspace func to filter unique workspace names

--- contracts/WorkSpace.sol
+++ contracts/WorkSpace.sol
@@ -21,38 +21,77 @@ contract WorkSpace{
     event VideoAdded(uint256 workspaceId, string videoId);
     event ParticipantAdded(uint256 workspaceId, address participant);
 
-    function createWorkspace(string memory _name,address  _initialParticipant, string memory _videoId) public {
-        uint256 newWorkspaceId = workspaceCount;
-    
-    // Create a new Video_WorkSpace struct and push it to the array
-    workspaces.push(Video_WorkSpace({
-        name: _name,
-        workspace_creator: msg.sender,
-        participants: new address[](0),
-        videoIds: new string[](0)
-    }));
+    function createWorkspace(string memory _name, address _initialParticipant, string memory _videoId) public {
+        uint256 existingWorkspaceId = findWorkspaceByName(_name);
 
-    // Add the initial participant (creator)
-    workspaces[newWorkspaceId].participants.push(_initialParticipant);
-    participantWorkspaces[_initialParticipant].push(newWorkspaceId);
-    workspaces[newWorkspaceId].videoIds.push(_videoId);
+        if (existingWorkspaceId != type(uint256).max) {
+            // Workspace with this name already exists
+            require(
+                msg.sender == workspaces[existingWorkspaceId].workspace_creator || 
+                isParticipant(existingWorkspaceId, msg.sender),
+                "Only creator or participants can add videos to existing workspace"
+            );
+            
+            // Add the video to the existing workspace
+            workspaces[existingWorkspaceId].videoIds.push(_videoId);
+            
+            emit VideoAdded(existingWorkspaceId, _videoId);
+        } else {
+            // Create a new workspace
+            uint256 newWorkspaceId = workspaceCount;
+            
+            workspaces.push(Video_WorkSpace({
+                name: _name,
+                workspace_creator: msg.sender,
+                participants: new address[](0),
+                videoIds: new string[](0)
+            }));
 
-    // Update creator information
-    creators[msg.sender].creatorAddress = msg.sender;
-    creators[msg.sender].workspaceIds.push(newWorkspaceId);
+            // Add the initial participant (creator)
+            workspaces[newWorkspaceId].participants.push(_initialParticipant);
+            participantWorkspaces[_initialParticipant].push(newWorkspaceId);
+            workspaces[newWorkspaceId].videoIds.push(_videoId);
 
-    workspaceCount++;
+            // Update creator information
+            creators[msg.sender].creatorAddress = msg.sender;
+            creators[msg.sender].workspaceIds.push(newWorkspaceId);
 
-    emit WorkspaceCreated(newWorkspaceId, _name, msg.sender);
-    emit ParticipantAdded(newWorkspaceId, _initialParticipant);
+            workspaceCount++;
+
+            emit WorkspaceCreated(newWorkspaceId, _name, msg.sender);
+            emit ParticipantAdded(newWorkspaceId, _initialParticipant);
+            emit VideoAdded(newWorkspaceId, _videoId);
+        }
     }
 
     function addVideoToWorkspace(uint256 _workspaceId, string memory _videoId) public {
         require(_workspaceId < workspaces.length, "Workspace does not exist");
-        require(msg.sender == workspaces[_workspaceId].workspace_creator, "Only creator can add videos");
-
+        require(
+            msg.sender == workspaces[_workspaceId].workspace_creator || 
+            isParticipant(_workspaceId, msg.sender),
+            "Only creator or participants can add videos"
+        );
         workspaces[_workspaceId].videoIds.push(_videoId);
         emit VideoAdded(_workspaceId, _videoId);
+    }
+
+    // Helper function to check if an address is a participant of a workspace
+    function isParticipant(uint256 _workspaceId, address _address) internal view returns (bool) {
+        for (uint i = 0; i < workspaces[_workspaceId].participants.length; i++) {
+            if (workspaces[_workspaceId].participants[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function findWorkspaceByName(string memory _name) internal view returns (uint256) {
+        for (uint256 i = 0; i < workspaceCount; i++) {
+            if (keccak256(bytes(workspaces[i].name)) == keccak256(bytes(_name))) {
+                return i;
+            }
+        }
+        return type(uint256).max; // Return max uint256 if not found
     }
     function addParticipantToWorkspace(uint256 _workspaceId, address _participant) public {
         require(_workspaceId < workspaces.length, "Workspace does not exist");
